918ddf385ed4929c7f046cd0dc463e77273b297f
Aleksandr Sazhin <i4j5@ya.ru>
2019-11-25 09:50:25 +0300

    -

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 
+use \Curl\Curl;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,13 +19,93 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::any('send', function () {
+    
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $order = $_POST['order'];
+    $utm_medium = $_POST['utm_medium'];
+    $utm_source = $_POST['utm_source'];
+    $utm_campaign = $_POST['utm_campaign'];
+    $utm_term = $_POST['utm_term'];
+    $utm_content = $_POST['utm_content'];
+    $comment = $_POST['comment'];
+    $url = $_POST['url'];
+    
+     
+    $comment = $comment . 
+        "<br>
+        <b>$order</b> <br>
+        Имя: $name <br>
+        Телефон: $phone <br>
+        E-mail: $email <br>
+        Страница захвата: $url <br> 
+        Ключевое слово: $utm_term <br>
+        ";
+
+    $postData = [
+        'TITLE' => $order,
+        'NAME' => $name,
+        'PHONE_MOBILE' => $phone,
+        
+        'LOGIN' => env('CRM_LOGIN'),
+        'PASSWORD' => env('CRM_PASSWORD'),
+        
+        'UTM_CAMPAIGN' => $utm_campaign,	
+        'UTM_CONTENT' => $utm_content,
+        'UTM_MEDIUM' => $utm_medium,
+        'UTM_SOURCE' => $utm_source,
+        'UTM_TERM' => $utm_term,
+        
+        'COMMENTS' => $comment,
+    ];
+    
+    if (preg_match("/^(?:[a-z0-9]+(?:[-_.]?[a-z0-9]+)?@[a-z0-9_.-]+(?:\.?[a-z0-9]+)?\.[a-z]{2,5})$/i", $email))
+    {
+      $postData['EMAIL_HOME'] = $email;
+    }
+    
+    $fp = fsockopen("ssl://" . env('CRM_HOST'), env('CRM_PORT'), $errno, $errstr, 30);
+    
+    if ($fp) {
+        $strPostData = '';
+        foreach ($postData as $key => $value)
+            $strPostData .= ($strPostData == '' ? '' : '&') . $key . '=' . urlencode($value);
+
+        $str = "POST " . env('CRM_PATH') . " HTTP/1.0\r\n";
+        $str .= "Host: " . env('CRM_HOST') . "\r\n";
+        $str .= "Content-Type: application/x-www-form-urlencoded\r\n";
+        $str .= "Content-Length: " . strlen($strPostData) . "\r\n";
+        $str .= "Connection: close\r\n\r\n";
+
+        $str .= $strPostData;
+
+        fwrite($fp, $str);
+
+        $result = '';
+        while (!feof($fp)) {
+            $result .= fgets($fp, 128);
+        }
+        fclose($fp);
+
+        $response = explode("\r\n\r\n", $result);
+
+        $output = '<pre>' . print_r($response[1], 1) . '</pre>';
+        
+        return $output;
+    } else {
+        return 'Connection Failed! ' . $errstr . ' (' . $errno . ')';
+    }
+
+    return 'ok';
+});
+
 
 // WebHooks
 Route::prefix('webhook')->group(function () {
     
     Route::any('test', function () {
-        $amo = \App\AmoCRM::getInstance();
-        dd($amo);
         return 'ok!';
     });
    
