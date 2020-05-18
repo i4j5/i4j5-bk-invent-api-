68e44b3d7f167b82c9f411c99d601d58a0ff9a6c
i4j5 <i4j5@ya.ru>
2020-05-18 16:50:34 +0300

    pages

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -3,8 +3,15 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-12 col-md-3 col-xl-2 bd-sidebar">
-            {!! $sidebar !!}
+        <div class="col-12 col-md-3 col-xl-2">
+            <div class="bd-sidebar-wrapper">
+                <button class="btn d-md-none collapsed" type="button" data-toggle="collapse" data-target="#bd-sidebar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" role="img" focusable="false">
+                        <title>Menu</title><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M4 7h22M4 15h22M4 23h22"></path>
+                    </svg>
+                </button>
+                <div class="bd-sidebar collapse" id="bd-sidebar">{!! $sidebar !!}</div>
+            </div>
         </div>
         <div class="col-12 col-md-9 col-xl-10 bd-content">
             <h1>{{ $page->name }}</h1>
